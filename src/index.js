b0994b83c4d6a33442b5f43a80a226e47beb5a81
Marek Dzwonnik <mardzw7@gmail.com>
2018-12-14 09:13:02 +0100

    dodany preventdefault

--- src/index.js
+++ src/index.js
@@ -6,29 +6,34 @@ const apiKey = `&APPID=254f5cfa909ce84a6c0a52b555b0ce5c`;
 const units = `&units=metric`;
 
 const loadWeather = () => {
-    const input = document.querySelector( '.search__input' ).value;
-    const url = `${api}${input}${units}${apiKey}`;
-    console.log( url );
+  const input = document.querySelector('.search__input').value;
+  const url = `${api}${input}${units}${apiKey}`;
+  console.log(input);
 
-    fetch( url )
-        .then( res => {
-            res.json()
+  fetch(url)
+    .then(res => {
+      res.json();
 
-            if ( !res.ok ) {
-                throw Error( res.statusText );
-            }
-        } )
-        .then( data => {
-            console.log( data );
-        } )
-        .catch( error => {
-            console.log(error);
-        });
-}
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 
 const setup = () => {
-    const button = document.getElementById( 'search__button' );
-    if (button ) { button.addEventListener( 'click', loadWeather ); }
-}
+  const button = document.getElementById('search__button');
+  if (button) {
+    button.addEventListener('click', event => {
+      event.preventDefault();
+      loadWeather();
+    });
+  }
+};
 
 setup();
